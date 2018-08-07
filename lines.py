22e622e2041edd7ceba5ba944e1df1ad70f15c23
easylistbrasil <42122189+easylistbrasil@users.noreply.github.com>
2018-08-07 00:00:59 -0300

    Add files via upload

---
+++ lines.py
@@ -0,0 +1,8 @@
+﻿#!/usr/bin/env python3
+
+with open("easylistbrasil.txt","r") as f, open("easylistbrasil.txt","w") as outfile:
+ for i in f.readlines():
+       if not i.strip():
+           continue
+       if i:
+           outfile.write(i)   
