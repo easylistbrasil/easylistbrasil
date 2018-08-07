5229380c2fc1e15e0d7e18ea70b9590945ab9ff0
easylistbrasil <42122189+easylistbrasil@users.noreply.github.com>
2018-08-07 00:19:22 -0300

    Delete lines.py

--- lines.py
+++
@@ -1,8 +0,0 @@
-﻿#!/usr/bin/env python3
-
-with open("easylistbrasil.txt","r") as f, open("easylistbrasil.txt","w") as outfile:
- for i in f.readlines():
-       if not i.strip():
-           continue
-       if i:
-           outfile.write(i)   
